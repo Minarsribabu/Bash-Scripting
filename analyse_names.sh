97d359431f47f36cfd16dc61b55c29d3da3c5af6
Minarsribabu <minarsriballa@gmail.com>
2025-11-10 22:20:27 +0530

    files added

---
+++ analyse_names.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+report="/c/Users/minar/DEVOPS/analyse_report.txt"
+echo -e "\nAnalysing Files " > "$report"
+echo -e "\nFiles modified/updated with in 24hrs" >> "$report"
+log_file=$(find . -name "*.txt" -mtime -1)
+Search=("Cse" "IT" "eee" "ece")
+
+
+echo -e "\nfiles are to be check is $log_file"  >> "$report"
+
+for log in $log_file;do
+	echo "$log" >> "$report"
+	for dept in "${Search[@]}";do
+
+	echo -e "\nfiles which contains $dept" >> "$report"
+	grep -i "$dept" "$log" >> "$report"
+	echo -e "\nno.of names that belongs to $dept" >> "$report"
+	grep -ci "$dept" "$log" >> "$report"
+
+	done
+done
